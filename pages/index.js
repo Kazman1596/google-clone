1fa5fe4918ba7bcb1668eae68d496a183b52a76a
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2022-09-03 10:47:14 -0700

    fixing home links and deployment

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,12 @@ export default function Home() {
     if(!term.trim()) return
     router.push(`/search?term=${term.trim()}&searchType=`)
   }
+  async function randomSearch(e){
+    e.preventDefault()
+    const randomTerm = await fetch('https://random-word-api.herokuapp.com/word').then((response) => response.json())
+    if(!randomTerm) return
+    router.push(`/search?term=${randomTerm}&searchType=`)
+  }
 
   return (
     <div>
@@ -43,7 +49,7 @@ export default function Home() {
         </div>
         <div className='flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center'>
           <button onClick={search} className='btn'>Google Search</button>
-          <button className='btn'>I&apos;m feeling lucky</button>
+          <button onClick={randomSearch} className='btn'>I&apos;m feeling lucky</button>
         </div>
       </form>
 
